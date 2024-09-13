4d4ec974477b2e42ab03d5f5604d970c98e1f4d5
roychandu <cr717753@gmail.com>
2024-09-13 08:21:25 +0530

    add scrollbar inside search field

--- Dictionary app/script.js
+++ Dictionary app/script.js
@@ -2,7 +2,9 @@ const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 const search_btn = document.querySelector("#search");
 const inp_word = document.querySelector("#inp_word");
 const volume = document.querySelector(".fi-ss-volume");
+const meaning_part = document.querySelector(".row2");
 
+volume.style.opacity = "0"
 
 search_btn.addEventListener("click", () =>{
     let inp_text = document.querySelector("#inp_text").value;
@@ -12,6 +14,14 @@ search_btn.addEventListener("click", () =>{
             console.log(data);
             inp_word.innerHTML = data[0].word;
             volume.style.opacity = "1"
+
+           meaning_part.innerHTML = "";
+           for(let i = 0; i < data.length; i++){
+                const meanings = document.createElement("div");
+                meanings.className = "meanings";
+
+
+           }
         })
         .catch(() =>{
             inp_word.innerHTML = "word not found";
